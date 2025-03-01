bc8f706dfa2206dfebbd64b4fe4d15ec1ebfbbf6
kimijunwoo <seonmok5@gmail.com>
2025-03-01 02:35:16 +0000

    modified:   game.css

---
+++ game.js
@@ -0,0 +1,36 @@
+var board = [
+    [0,0,0];
+    [0,0,0];
+    [0,0,0];
+]
+
+const ME = 1;
+const AI = -1;
+
+function evaluate(){
+
+}
+
+function gameOver(){
+
+}
+
+function emptyCells(){
+
+}
+
+function setMove(row =, col, player){
+
+}
+
+function minimax(depth, isMaximizig){
+
+}
+
+function AIturn(){
+
+}
+
+function ClikedCell(event){
+    
+}
